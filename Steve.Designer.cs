823122f5b183f3f60349e5b1f1dceabe5352a6c3
Sam <samuelturnerlill@gmail.com>
2019-08-05 23:41:39 -0500

    Add project files.

---
+++ Steve.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Steve
+{
+    partial class Steve
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbCpuUtilization = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbCpuUtilization)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbCpuUtilization
+            // 
+            this.tbCpuUtilization.Location = new System.Drawing.Point(12, 51);
+            this.tbCpuUtilization.Maximum = 100;
+            this.tbCpuUtilization.Name = "tbCpuUtilization";
+            this.tbCpuUtilization.Size = new System.Drawing.Size(341, 45);
+            this.tbCpuUtilization.TabIndex = 0;
+            this.tbCpuUtilization.TickFrequency = 5;
+            this.tbCpuUtilization.ValueChanged += new System.EventHandler(this.TbCpuUtilization_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CPU Utilization";
+            // 
+            // Steve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbCpuUtilization);
+            this.Name = "Steve";
+            this.Text = "Steve";
+            ((System.ComponentModel.ISupportInitialize)(this.tbCpuUtilization)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbCpuUtilization;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Steve.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steve
+{
+    public partial class Steve : Form
+    {
+        #region Member Variables..
+        private CancellationTokenSource _CpuTokenSource;
+        #endregion Member Variables..
+
+        #region Handlers/Delegates
+        //public delegate void OnCpuValueChanged(int value, CancellationToken ct);
+        #endregion Handlers/Delegates
+
+        #region Constructors..
+        #region Steve
+        public Steve()
+        {
+            InitializeComponent();
+
+            InitializeLoadCpu();
+        }
+        #endregion Steve
+        #endregion Constructors..
+
+        #region Methods..
+
+        #region Events..
+        #region TbCpuUtilization_ValueChanged
+        private void TbCpuUtilization_ValueChanged(object sender, EventArgs e)
+        {
+            _CpuTokenSource.Cancel();
+            _CpuTokenSource = new CancellationTokenSource();
+
+            LoadCpu(((TrackBar) sender).Value, _CpuTokenSource.Token);
+        }
+        #endregion TbCpuUtilization_ValueChanged
+        #endregion Events..
+
+        #region InitializeLoadCpu
+        private void InitializeLoadCpu()
+        {
+            _CpuTokenSource = new CancellationTokenSource();
+            LoadCpu(0, _CpuTokenSource.Token);
+        }
+        #endregion InitializeLoadCpu
+
+        private void LoadCpu(int percentage, CancellationToken ct)
+        {
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    Stopwatch Watch = new Stopwatch();
+                    Watch.Start();
+
+                    while (true)
+                    {
+                        // Make the loop go on for "percentage" milliseconds then sleep the 
+                        // remaining percentage milliseconds. So 40% utilization means work 40ms and sleep 60ms
+                        if (Watch.ElapsedMilliseconds > percentage)
+                        {
+                            Thread.Sleep(100 - percentage);
+                            Watch.Reset();
+                            Watch.Start();
+                        }
+
+                        ct.ThrowIfCancellationRequested();
+                    }
+                }
+                catch (OperationCanceledException) { }
+            }, ct);
+        }
+        #endregion Methods
+    }
+}
