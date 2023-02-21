5618a86b7c925ab05106552d7c51aa55c24146b8
sLill <SamuelTurnerLill@gmail.com>
2023-02-21 16:29:09 -0600

    -

---
+++ Steve/Steve.ImportantFunctions/VeryImportant.cs
@@ -0,0 +1,12 @@
+﻿namespace Steve.ImportantFunctions
+{
+    public static class VeryImportant
+    {
+        #region Methods..
+        public static string GetImportantText()
+        {
+            return "When does a pond become a lake?";
+        }
+        #endregion Methods..
+    }
+}
